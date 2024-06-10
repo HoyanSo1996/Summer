79636df0aeaba7c3422d0bc3f50290b95e608245
KennySo <HoyanSo1996@163.com>
2024-06-11 00:55:34 +0800

    Initialization.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Summer
+一个仿写 Spring 容器的 Demo
---
+++ src/main/java/com/omega/Main.java
@@ -0,0 +1,13 @@
+package com.omega;
+
+/**
+ * Class ${NAME}
+ *
+ * @author KennySo
+ * @date 2024/6/11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
